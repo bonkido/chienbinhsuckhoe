45fb64acfae6fe0d76fb0db90ac84436d4260aca
Thien Phu <bonbonbon2k4@gmail.com>
2024-05-23 19:22:50 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  providers: [DatePipe], // Đăng ký DatePipe như một provider của component
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SucKhoe';
+export class AppComponent implements OnInit {
+  currentTime: string = '';
+
+  constructor(private datePipe: DatePipe) {}
+
+  ngOnInit() {
+    this.updateTime();
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  }
+
+  updateTime() {
+    const formattedTime = this.datePipe.transform(new Date(), 'yyyy-MM-dd HH:mm:ss');
+    this.currentTime = formattedTime !== null ? formattedTime : '';
+  }
 }
